eac9c297c367765e249e7ee8f4034b0568d8f6f6
hansderie <hderie@hotmail.com>
2023-10-03 14:53:34 +0200

    oefeningen

---
+++ gemiddelde.mjs
@@ -0,0 +1,14 @@
+import * as readline from 'node:readline/promises';
+import{exit, stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+
+let getal  = parseInt(await userInput.question('Wat is getal? '));
+getal = getal + parseInt(await userInput.question('Wat is getal 2? '));
+getal = getal + parseInt(await userInput.question('Wat is getal 3? '));
+getal = getal + parseInt(await userInput.question('Wat is getal 4? '));
+
+console.log(getal/4);
+
+process.exit()
+
--- seconds.mjs
+++ seconds.mjs
@@ -2,12 +2,14 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
-let getal1 = parseFloat(await userInput.question('Wat is aantal dagen? '));
-let getal2 = parseFloat(await userInput.question('Wat is aantal uren? '));
-let getal3 = parseFloat(await userInput.question('Wat is aantal minuten? '));
-let getal4 = parseFloat(await userInput.question('Wat is aantal seconden? '));
+let dagen = parseInt(await userInput.question('Wat is aantal dagen? '));
+let uren = parseInt(await userInput.question('Wat is aantal uren? '));
+let minuten = parseInt(await userInput.question('Wat is aantal minuten? '));
+let seconden = parseInt(await userInput.question('Wat is aantal seconden? '));
 
-console.log('totaal aantal seconden is '+ (getal4+(getal3*60)+(getal2*3600)+(getal1*(3600*24))));
+let totaalSeconden = (((dagen*24) + uren)*60 + minuten) * 60 + seconden
+
+console.log('totaal aantal seconden is '+ totaalSeconden);
 
 
 
